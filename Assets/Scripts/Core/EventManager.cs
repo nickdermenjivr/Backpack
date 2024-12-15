f2003d690d3f14c974a488c6ea12c3e1b2199cd0
nickdermenjivr <nickdermenji@gmail.com>
2024-12-15 12:21:10 +0200

    Add EventManager script for event handling

---
+++ Assets/Scripts/Core/EventManager.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+
+namespace Core
+{
+    public class EventManager
+    {
+        private static readonly Dictionary<Type, List<Delegate>> EventDictionary = new();
+
+        public static void Subscribe<T>(Action<T> listener)
+        {
+            var eventType = typeof(T);
+
+            if (!EventDictionary.ContainsKey(eventType))
+            {
+                EventDictionary[eventType] = new List<Delegate>();
+            }
+
+            EventDictionary[eventType].Add(listener);
+        }
+        
+        public static void Unsubscribe<T>(Action<T> listener)
+        {
+            var eventType = typeof(T);
+
+            if (!EventDictionary.TryGetValue(eventType, out var value)) return;
+            value.Remove(listener);
+            
+            if (EventDictionary[eventType].Count == 0)
+            {
+                EventDictionary.Remove(eventType);
+            }
+        }
+
+        public static void TriggerEvent<T>(T eventData)
+        {
+            var eventType = typeof(T);
+
+            if (!EventDictionary.TryGetValue(eventType, out var value)) return;
+            
+            foreach (var listener in value)
+            {
+                (listener as Action<T>)?.Invoke(eventData);
+            }
+        }
+    }
+}
